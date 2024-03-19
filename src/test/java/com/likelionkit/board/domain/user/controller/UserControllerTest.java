845b3d401e97657da9f22b6d46d1f117d8f5da27
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-03-19 23:09:42 +0900

    fix : Nested로 테스트 묶기

--- src/test/java/com/likelionkit/board/domain/user/controller/UserControllerTest.java
+++ src/test/java/com/likelionkit/board/domain/user/controller/UserControllerTest.java
@@ -6,6 +6,7 @@ import com.likelionkit.board.domain.user.service.UserService;
 import com.likelionkit.board.global.base.exception.CustomException;
 import com.likelionkit.board.global.base.exception.ErrorCode;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -33,63 +34,67 @@ public class UserControllerTest {
     @MockBean
     private UserService userService;
 
-    @DisplayName("회원가입이 성공함.")
-    @Test
-    void 회원가입_성공() throws Exception {
-        // given
-        SignUpRequest request = new SignUpRequest("test","123456");
+    @Nested
+    @DisplayName("<회원가입>")
+    class signUpTest {
+        @DisplayName("회원가입이 성공함.")
+        @Test
+        void 회원가입_성공() throws Exception {
+            // given
+            SignUpRequest request = new SignUpRequest("test","123456");
 
-        //when -> then
-        mockMvc.perform(post("/api/users/sign-up")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(request)))
-                //then
-                .andExpect(status().isCreated())
-                .andDo(print());
-    }
+            //when -> then
+            mockMvc.perform(post("/api/users/sign-up")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+                    //then
+                    .andExpect(status().isCreated())
+                    .andDo(print());
+        }
 
-    @Test
-    void 회원가입시_이미_존재하는_userName이면_실패() throws Exception{
-        // given
-        SignUpRequest request = new SignUpRequest("test","123456");
-        doThrow(new CustomException(ErrorCode.DUPLICATED_USER_NAME))
-                .when(userService).signUp(any(SignUpRequest.class));
+        @Test
+        void 회원가입시_이미_존재하는_userName이면_실패() throws Exception{
+            // given
+            SignUpRequest request = new SignUpRequest("test","123456");
+            doThrow(new CustomException(ErrorCode.DUPLICATED_USER_NAME))
+                    .when(userService).signUp(any(SignUpRequest.class));
 
-        // when -> then
-        mockMvc.perform(post("/api/users/sign-up")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request)))
-                //then
-                .andExpect(status().isConflict());
-    }
+            // when -> then
+            mockMvc.perform(post("/api/users/sign-up")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+                    //then
+                    .andExpect(status().isConflict());
+        }
 
-    @Test
-    void 회원가입시_빈값이_들어오면_실패() throws Exception{
-        SignUpRequest request = new SignUpRequest("","123456");
+        @Test
+        void 회원가입시_빈값이_들어오면_실패() throws Exception{
+            SignUpRequest request = new SignUpRequest("","123456");
 
-        // when -> then
-        mockMvc.perform(post("/api/users/sign-up")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request)))
-                //then
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
-                .andExpect(jsonPath("$.message").value("사용자 이름은 빈칸일 수 없습니다."));
-    }
+            // when -> then
+            mockMvc.perform(post("/api/users/sign-up")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+                    //then
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                    .andExpect(jsonPath("$.message").value("사용자 이름은 빈칸일 수 없습니다."));
+        }
 
-    @ValueSource(strings = {"12","12345","1234567891011"})
-    @ParameterizedTest
-    void 회원가입시_비밀번호_길이가_6보다작거나_12보다크면_실패(String password) throws Exception{
-        SignUpRequest request = new SignUpRequest("test", password);
+        @ValueSource(strings = {"12","12345","1234567891011"})
+        @ParameterizedTest
+        void 회원가입시_비밀번호_길이가_6보다작거나_12보다크면_실패(String password) throws Exception{
+            SignUpRequest request = new SignUpRequest("test", password);
 
-        // when -> then
-        mockMvc.perform(post("/api/users/sign-up")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request)))
-                //then
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
-                .andExpect(jsonPath("$.message").value("비밀번호는 6자리 이상, 12자리 이하여야 합니다."))
-                .andDo(print());
+            // when -> then
+            mockMvc.perform(post("/api/users/sign-up")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+                    //then
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                    .andExpect(jsonPath("$.message").value("비밀번호는 6자리 이상, 12자리 이하여야 합니다."))
+                    .andDo(print());
+        }
     }
 }
