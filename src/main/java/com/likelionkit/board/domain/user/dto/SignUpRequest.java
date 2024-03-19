b8c64dc507e3a59dc5499ecd6182b11601a28388
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-03-19 23:07:54 +0900

    add : 회원가입 테스트 작성

--- src/main/java/com/likelionkit/board/domain/user/dto/SignUpRequest.java
+++ src/main/java/com/likelionkit/board/domain/user/dto/SignUpRequest.java
@@ -3,16 +3,18 @@ package com.likelionkit.board.domain.user.dto;
 import com.likelionkit.board.domain.user.model.User;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class SignUpRequest {
 
     @NotBlank(message = "사용자 이름은 빈칸일 수 없습니다.")
     private String userName;
 
     @NotBlank(message = "비밀번호는 빈칸일 수 없습니다.")
-    @Size(min = 6, max = 12)
+    @Size(min = 6, max = 12, message = "비밀번호는 6자리 이상, 12자리 이하여야 합니다.")
     private String password;
 
     public static User toEntity(SignUpRequest request) {
