c20c87ca2576d9686717e73324b9f6a674979b03
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-03-23 04:24:17 +0900

    add : UserDetails, UserDetailsService 구현체 작성

---
+++ src/main/java/com/likelionkit/board/domain/user/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.likelionkit.board.domain.user.service;
+
+import com.likelionkit.board.domain.user.model.User;
+import com.likelionkit.board.domain.user.model.UserPrincipal;
+import com.likelionkit.board.domain.user.repository.UserRepository;
+import com.likelionkit.board.global.base.exception.CustomException;
+import com.likelionkit.board.global.base.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUserName(username)
+                .map(UserPrincipal::new)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER_NAME));
+    }
+}
